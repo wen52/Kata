7e3e79db26d36d8971194b411d4e0f2b9cf34f39
Cloud <cloud.chen@titansoft.com.sg>
2020-09-06 13:52:57 +0800

    add SumStringsService

---
+++ Kata/SumStringsService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kata
+{
+    public class SumStringsService
+    {
+        public string Add(string a, string b)
+        {
+            long aNum = 0;
+            long bNum = 0;
+            if (a != "")
+            {
+                aNum = Convert.ToInt64(a);
+            }
+            if (b != "")
+            {
+                bNum = Convert.ToInt64(b);
+            }
+            var result = aNum + bNum;
+
+            return result.ToString();
+        }
+    }
+}
---
+++ KataTests/SumStringsTest.cs
@@ -0,0 +1,17 @@
+﻿using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+    [TestClass]
+    public class SumStringsTest
+    {
+        [TestMethod]
+        public void Testcase()
+        {
+            var sumStringsService = new SumStringsService();
+            var result = sumStringsService.Add("123", "456");
+            Assert.AreEqual("579",result);
+        } 
+    }
+}
