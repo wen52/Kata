409147c85378be033ff65f0446319daa3c461026
Cloud <cloud.chen@titansoft.com.sg>
2020-09-06 13:26:06 +0800

    add CountDuplicateService.cs

---
+++ Kata/CountDuplicateService.cs
@@ -0,0 +1,25 @@
+﻿namespace Kata
+{
+    public class CountDuplicateService
+    {
+        public int count(string inputString)
+        {
+            int result = 0;
+            var inputArray = inputString.ToUpper().ToCharArray();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = i + 1; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i] == inputArray[j] && inputArray[i] != ' ')
+                    {
+                        result++;
+                        inputString = inputString.ToUpper().Replace(inputArray[i], ' ');
+                        inputArray = inputString.ToCharArray();
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ KataTests/CountDuplicateTests.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+using Kata;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KataTests
+{
+    [TestClass]
+    public class CountDuplicateTests
+    {
+        [TestMethod]
+        public void NoDuplicate_ShouldReturnZero()
+        {
+            var countDuplicateService = new CountDuplicateService();
+            var result = countDuplicateService.count("abcde");
+            Assert.AreEqual(0,result);
+        }
+        [TestMethod]
+        public void aabbcde_ShouldReturnTwo()
+        {
+            var countDuplicateService = new CountDuplicateService();
+            var result = countDuplicateService.count("aabbcde");
+            Assert.AreEqual(2, result);
+        }
+        [TestMethod]
+        public void aabBcde_ShouldReturnTwo()
+        {
+            var countDuplicateService = new CountDuplicateService();
+            var result = countDuplicateService.count("aabBcde");
+            Assert.AreEqual(2, result);
+        }
+        [TestMethod]
+        public void indivisibility_ShouldReturnOne()
+        {
+            var countDuplicateService = new CountDuplicateService();
+            var result = countDuplicateService.count("indivisibility");
+            Assert.AreEqual(1, result);
+        }
+        [TestMethod]
+        public void Indivisibilities_ShouldReturnTwo()
+        {
+            var countDuplicateService = new CountDuplicateService();
+            var result = countDuplicateService.count("Indivisibilities");
+            Assert.AreEqual(2, result);
+        }
+        [TestMethod]
+        public void aA11_ShouldReturnTwo()
+        {
+            var countDuplicateService = new CountDuplicateService();
+            var result = countDuplicateService.count("aA11");
+            Assert.AreEqual(2, result);
+        }
+        [TestMethod]
+        public void ABBA_ShouldReturnTwo()
+        {
+            var countDuplicateService = new CountDuplicateService();
+            var result = countDuplicateService.count("ABBA");
+            Assert.AreEqual(2, result);
+        }
+    }
+}
